39c9f6b6c597ba156a070ff001a1a9735cbedfd8
Costas Kounadis <compkos@gmail.com>
2020-05-08 13:13:11 +0000

    2020-05-07 16:10

--- datacamppandas.py
+++ datacamppandas.py
@@ -25,7 +25,36 @@ cars.index=row_labels
 print(cars)
 
 # Import the cars.csv data: cars
-cars=pd.read_csv('cars.csv')
+cars=pd.read_csv('cars.csv', index_col=0)
 
 # Print out cars
 print(cars)
+
+# Print out country column as Pandas Series
+print(cars['country'])
+
+# Print out country column as Pandas DataFrame
+print(cars[['country']])
+
+# Print out DataFrame with country and drives_right columns
+print(cars[['country','drives_right']])
+
+print(cars[0:3])
+
+# Print out fourth, fifth and sixth observation
+print(cars.iloc[[2,3,4]])
+
+# Print out drives_right value of Morocco
+print(cars.loc['MOR','drives_right'])
+
+# Print sub-DataFrame
+print(cars.loc[['MOR','RU'],['country','drives_right']])
+
+# Print out drives_right column as Series
+print(cars.loc[:,'drives_right'])
+
+# Print out drives_right column as DataFrame
+print(cars.loc[:,['drives_right']])
+
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.loc[:,['cars_per_cap','drives_right']])
