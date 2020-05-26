2e0081004475bb5316ba1ebac8f3d27e2bd9babd
Costas Kounadis <compkos@gmail.com>
2020-05-26 13:44:00 +0000

    2020-05-26 16:40

--- datacamppandas.py
+++ datacamppandas.py
@@ -226,3 +226,163 @@ result = high_homelessness_srt[['state','indiv_per_10k']]
 
 # See the result
 print(result)
+
+sales_list=[[1    ,'A'           ,1 ,'2010-02-05'      ,24924.50       ,False,          5.728                 ,0.679         ,8.106],
+[1    ,'A'           ,1 ,'2010-03-05'      ,21827.90       ,False,          8.056                 ,0.693         ,8.106],
+[1    ,'A'           ,1 ,'2010-04-02'      ,57258.43       ,False,         16.817                 ,0.718         ,7.808],
+[1    ,'A'           ,1 ,'2010-05-07'      ,17413.94       ,False,         22.528                 ,0.749         ,7.808],
+[1    ,'A'           ,1 ,'2010-06-04'      ,17558.09       ,False,         27.050                 ,0.715         ,7.808],
+[ 2    ,'A'           ,1 ,'2010-02-05'      ,35034.06       ,False,          4.550                 ,0.679         ,8.324],
+[ 4    ,'A'           ,1 ,'2010-02-05'      ,38724.42       ,False,          6.533                 ,0.686         ,8.623],
+[ 6    ,'A'           ,1 ,'2010-02-05'      ,25619.00       ,False,          4.683                 ,0.679         ,7.259],
+[10    ,'B'           ,1 ,'2010-02-05'      ,40212.84       ,False,         12.411                 ,0.782         ,9.765]
+
+]
+
+sales=pd.DataFrame(sales_list)
+sales.columns = ['store', 'type','department','date',  'weekly_sales',  'is_holiday',  'temperature_c',  'fuel_price_usd_per_l',  'unemployment']
+print(sales)
+
+# Drop duplicate store/type combinations
+store_types = sales.drop_duplicates(subset=["store","type"])
+print(store_types.head())
+
+# Drop duplicate store/department combinations
+store_depts = sales.drop_duplicates(subset=["store","department"])
+print(store_depts.head())
+
+# Subset the rows that are holiday weeks and drop duplicate dates
+holiday_dates = sales[sales['is_holiday']==True].drop_duplicates(subset="date")
+
+# Print date col of holiday_dates
+print(holiday_dates['date'])
+
+storelist=[[ 1          ,'A'],
+[ 2          ,'A'],
+[ 4          ,'A'],
+[ 6          ,'A'],
+[10          ,'B'],
+[13          ,'A'],
+[14          ,'A'],
+[19          ,'A'],
+[20          ,'A'],
+[27          ,'A'],
+[31          ,'A'],
+[39          ,'A']]
+
+stores=pd.DataFrame(storelist)
+stores.columns = ['store_num', 'store_type']
+store_counts = stores["store_type"].value_counts()
+print(store_counts)
+
+departments_list=[[ 1             ,  1],
+[ 1             ,  2],
+[ 1             ,  3],
+[ 1             ,  4],
+[ 1             ,  5],
+[39             , 95],
+[39             , 96],
+[39             , 97],
+[39             , 98],
+[39             , 99]]
+
+departments=pd.DataFrame(departments_list)
+departments.columns = ['store_num','department_num']
+# Count the number of stores of each type                                                
+print(departments )                                                                      
+store_counts = stores["store_type"].value_counts()                                       
+print(store_counts)                                                                      
+                                                                                         
+# Get the proportion of stores of each type                                              
+store_props = stores["store_type"].value_counts(normalize=True)                          
+print(store_props)                                                                       
+                                                                                         
+# Count the number of each department number and sort                                    
+dept_counts_sorted = departments['department_num'].value_counts(sort=True)               
+print(dept_counts_sorted)                                                                
+                                                                                         
+# Get the proportion of departments of each number and sort                              
+dept_props_sorted = departments['department_num'].value_counts(sort=True, normalize=True)
+                                                                                       
+# Count the number of each department number and sort                                    
+dept_counts_sorted = departments['department_num'].value_counts(sort=True)               
+print(dept_counts_sorted)                                                                
+                                                                                         
+# Get the proportion of departments of each number and sort                              
+dept_props_sorted = departments['department_num'].value_counts(sort=True, normalize=True)
+
+# Calc total weekly sales
+sales_all = sales['weekly_sales'].sum()
+
+# Subset for type A stores, calc total weekly sales
+sales_A = sales[sales["type"] == "A"]['weekly_sales'].sum()
+
+# Subset for type B stores, calc total weekly sales
+sales_B = sales[sales["type"] == "B"]['weekly_sales'].sum()
+
+# Subset for type C stores, calc total weekly sales
+sales_C = sales[sales["type"] == "C"]['weekly_sales'].sum()
+
+# Get proportion for each type
+sales_propn_by_type = [sales_A, sales_B, sales_C] / sales_all
+print(sales_propn_by_type)
+
+# Group by type; calc total weekly sales
+sales_by_type = sales.groupby("type")["weekly_sales"].sum()
+
+# Get proportion for each type
+sales_propn_by_type = sales_by_type / sum(sales_by_type)
+print(sales_propn_by_type)
+
+# From previous step
+sales_by_type = sales.groupby("type")["weekly_sales"].sum()
+
+# Group by type and is_holiday; calc total weekly sales
+sales_by_type_is_holiday = sales.groupby(["type","is_holiday"])["weekly_sales"].sum()
+print(sales_by_type_is_holiday)
+
+# Import NumPy with the alias np
+import numpy as np
+
+# For each store type, aggregate weekly_sales: get min, max, mean, and median
+sales_stats = sales.groupby("type")["weekly_sales"].agg([np.min, np.max, np.mean, np.median])
+
+# Print sales_stats
+print(sales_stats)
+
+# For each store type, aggregate unemployment and fuel_price_usd_per_l: get min, max, mean, and median
+unemp_fuel_stats = sales.groupby("type")[["unemployment", "fuel_price_usd_per_l"]].agg([np.min, np.max, np.mean, np.median])
+
+# Print unemp_fuel_stats
+print(unemp_fuel_stats)
+
+# Pivot for mean weekly_sales for each store type
+mean_sales_by_type = sales.pivot_table(values=
+"weekly_sales", index="type")
+
+# Print mean_sales_by_type
+print(mean_sales_by_type)
+
+# Import NumPy as np
+import numpy as np
+
+# Pivot for mean and median weekly_sales for each store type
+mean_med_sales_by_type = sales.pivot_table(values="weekly_sales"
+, index="type", aggfunc=[np.mean, np.median])
+
+# Print mean_med_sales_by_type
+print(mean_med_sales_by_type)
+
+# Pivot for mean weekly_sales by store type and holiday 
+mean_sales_by_type_holiday = sales.pivot_table(values="weekly_sales"
+, index="type", columns='is_holiday')
+
+# Print mean_sales_by_type_holiday
+print(mean_sales_by_type_holiday)
+
+# Print mean weekly_sales by department and type; fill missing values with 0
+print(sales.pivot_table(values="weekly_sales"
+, index="department", columns="type",fill_value=0))
+
+# Print the mean weekly_sales by department and type; fill missing values with 0s; sum all rows and cols
+print(sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value=0, margins=True))
