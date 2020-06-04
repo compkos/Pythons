b0d4778e120da83bbb16815228f3eaf9c3ea7aaa
Costas Kounadis <compkos@gmail.com>
2020-06-04 18:25:55 +0000

    2020-06-04 21:30

--- mathesisregularexp.py
+++ mathesisregularexp.py
@@ -54,3 +54,51 @@ while True:
     found = re.findall(tag_pattern, html, re.I)
     found = list(set(found))
     for f in found :print('\t\t', f)
+
+# Μάθημα 19: Regular Expressions
+# 19.4 Παράδειγμα: Τα ελληνικά βουνά, έκδοση 2
+'''
+Στο παράδειγμα της ενότητας 17 (αρχεία), είχαμε επεξεργαστεί το
+αρχείο vouna.txt χρησιμοποιώντας μεθόδους συμβολοσειρών.
+Να επεκτείνετε την άσκηση ώστε να περιλάβετε τη γεωγραφική περιοχή
+κάθε βουνού, χρησιμοποιώντας regular expressions.
+'''
+with open('vouna.txt', 'r', encoding = 'utf-8') as f :
+    vouna = []
+    for vouno in f:
+        vouno = vouno.split('\t')
+        name = vouno[0]
+        height = vouno[1]
+        region = vouno[2]
+        vouna.append((name,height, region))
+for v in vouna:
+    print(v)
+
+max_height = 0
+for v in vouna:
+    if int(v[1]) > max_height: max_height = int(v[1])
+print('Το ψηλότερο βουνό έχει ύψος', max_height)
+to_file = ''
+with open('vouna2.txt', 'w', encoding='utf-8') as f:
+    for v in vouna:
+        #region = re.sub(r'\([^)]*\)', '', v[2]).split(',')
+        region = re.sub(r" ?\([^)]+\)", '', v[2]).split(',')
+        print(region)
+        to_file += 'Το όρος {} έχει ύψος {} μέτρα. '.format(v[0],v[1])
+        v_height = int(v[1])
+        if v_height == max_height:
+            to_file += "Είναι το ψηλότερο ελληνικό βουνό,"
+        else :
+            diff = max_height - v_height
+            to_file += "Είναι {} μ. χαμηλότερο,".format(diff)
+        ## Προσθήκη αναφοράς στην περιοχή
+        to_file += " και βρίσκεται στη "
+        for r in region:
+            r_name = r.rstrip()
+            if r_name[-1] == 'ς' : r_name = r_name[:-1]
+            if region.index(r) == 0 : kai = ' '+r_name
+            else: kai = ' και στη '+ r_name
+            to_file += kai
+        to_file += '.'
+    print(to_file)
+    f.write(to_file)
